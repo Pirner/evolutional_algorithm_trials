125df0aa766ec418ef14e6b472f6139e6810f43b
Nerolith <stephan.pirner93@gmail.com>
2024-06-02 10:09:19 +0200

    first version of genetics...

--- src/simulation_2d/game.py
+++ src/simulation_2d/game.py
@@ -17,12 +17,15 @@ class GameSimulation2D:
         self.width = 1536
         self.canvas = pygame.display.set_mode((self.width, self.height))
         self.strength = 30
-        self.up_strength_cap = 50
-        self.lo_strength_cap = 10
+        self.up_strength_cap = 45
+        self.lo_strength_cap = 5
 
         self.shoot_angle = 0
         self.up_angle = 90
         self.lo_angle = 0
+
+        self.target = None
+        self.all_sprites_list = None
 
         pygame.font.init()
         self.my_font = pygame.font.SysFont('Arial', self.strength)
@@ -60,13 +63,92 @@ class GameSimulation2D:
                 arrow.finished = False
                 arrow.shot = False
 
+    def all_arrows_finished(self) -> bool:
+        """
+        check if all arrows are finished
+        :return:
+        """
+        ret = True
+        for arr in self.arrows:
+            ret = ret and arr.finished
+        return ret
+
+    def generate_new_gen_of_arrows(self):
+        """
+        takes all the arrows and creates the next generation
+        :return:
+        """
+        self.arrows.sort(key=lambda x: x.score, reverse=True)
+        scores = [x.score for x in self.arrows]
+        print(scores)
+        n_arrows = len(scores)
+        new_gen_arrows = []
+        for arrow in self.arrows:
+            self.all_sprites_list.remove(arrow)
+
+        # take most fit bird and mutate it
+        for _ in range(n_arrows // 3):
+            arrow = Projectile(
+                velocity_x=15,
+                velocity_y=-20,
+                border_x=self.width,
+                border_y=self.height,
+                im_filepath='assets/arrow.png',
+                target=self.target,
+                up_streng_limit=self.up_strength_cap,
+                lo_strength_limit=self.lo_strength_cap,
+                up_angle_imit=self.up_angle,
+                lo_angle_limit=self.lo_angle,
+                male=self.arrows[0],
+                female=None,
+            )
+            new_gen_arrows.append(arrow)
+        for _ in range(n_arrows // 3):
+            arrow = Projectile(
+                velocity_x=15,
+                velocity_y=-20,
+                border_x=self.width,
+                border_y=self.height,
+                im_filepath='assets/arrow.png',
+                target=self.target,
+                up_streng_limit=self.up_strength_cap,
+                lo_strength_limit=self.lo_strength_cap,
+                up_angle_imit=self.up_angle,
+                lo_angle_limit=self.lo_angle,
+                male=self.arrows[1],
+                female=None,
+            )
+            new_gen_arrows.append(arrow)
+        for _ in range(n_arrows // 3):
+            arrow = Projectile(
+                velocity_x=15,
+                velocity_y=-20,
+                border_x=self.width,
+                border_y=self.height,
+                im_filepath='assets/arrow.png',
+                target=self.target,
+                up_streng_limit=self.up_strength_cap,
+                lo_strength_limit=self.lo_strength_cap,
+                up_angle_imit=self.up_angle,
+                lo_angle_limit=self.lo_angle,
+                male=self.arrows[0],
+                female=self.arrows[1],
+            )
+            new_gen_arrows.append(arrow)
+        self.arrows = new_gen_arrows
+        for arrow in self.arrows:
+            self.all_sprites_list.add(arrow)
+
     def start_mainloop(self):
         """
         start the mainloop of the game
         :return:
         """
+        use_ai = True
+        n_arrows = 50
+
         self.exit = False
-        all_sprites_list = pygame.sprite.Group()
+        self.all_sprites_list = pygame.sprite.Group()
 
         target = Target(
             pos_x=1300,
@@ -75,79 +157,96 @@ class GameSimulation2D:
             size_y=225,
             im_filepath='assets/target.png'
         )
-
-        arrow = Projectile(
-            velocity_x=15,
-            velocity_y=-20,
-            border_x=self.width,
-            border_y=self.height,
-            im_filepath='assets/arrow.png',
-            target=target,
-        )
-        self.arrows.append(arrow)
-        arrow.rotation_angle = self.shoot_angle
+        self.target = target
+        for _ in range(n_arrows):
+            arrow = Projectile(
+                velocity_x=15,
+                velocity_y=-20,
+                border_x=self.width,
+                border_y=self.height,
+                im_filepath='assets/arrow.png',
+                target=target,
+                up_streng_limit=self.up_strength_cap,
+                lo_strength_limit=self.lo_strength_cap,
+                up_angle_imit=self.up_angle,
+                lo_angle_limit=self.lo_angle,
+            )
+            self.arrows.append(arrow)
+            arrow.rotation_angle = self.shoot_angle
+            self.all_sprites_list.add(arrow)
         # arrow.rect.x = 0
         # arrow.rect.y = self.height - arrow.height * 2
-        all_sprites_list.add(target)
-        all_sprites_list.add(arrow)
+        self.all_sprites_list.add(target)
 
         clock = pygame.time.Clock()
 
         while not self.exit:
-            self.update_values()
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.exit = True
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_x:  # Pressing the x Key will quit the game
-                        self.exit = False
+            if use_ai:
+                # perform actions if genetic search is enabled
+                for arrow in self.arrows:
+                    if not arrow.shot:
+                        streng_val, angle_val = arrow.make_shot_decision()
+                        # fire arrow
+                        arrow.shot = True
+                        # set strength values from rotation and shoot angle
+                        vel_x, vel_y = Arrow2DMathematics.get_velocity_x_velocity_y(streng_val, angle_val)
+                        arrow.vel_x = vel_x
+                        arrow.vel_y = vel_y
+                if self.all_arrows_finished():
+                    print('finished all arrows')
+                    self.generate_new_gen_of_arrows()
 
-            keys = pygame.key.get_pressed()
+            else:
+                self.update_values()
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        self.exit = True
+                    elif event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_x:  # Pressing the x Key will quit the game
+                            self.exit = False
 
-            if keys[pygame.K_LEFT]:
-                if self.strength > self.lo_strength_cap:
-                    self.strength -= 1
-            if keys[pygame.K_RIGHT]:
-                if self.strength < self.up_strength_cap:
-                    self.strength += 1
-            if keys[pygame.K_SPACE]:
-                if not arrow.shot:
-                    # fire arrow
-                    arrow.shot = True
-                    # set strength values from rotation and shoot angle
-                    vel_x, vel_y = Arrow2DMathematics.get_velocity_x_velocity_y(self.strength, self.shoot_angle)
-                    arrow.vel_x = vel_x
-                    arrow.vel_y = vel_y
-            if keys[pygame.K_UP]:
-                if self.shoot_angle < self.up_angle:
-                    self.shoot_angle += 1
-                    arrow.rotation_angle = self.shoot_angle
-            if keys[pygame.K_DOWN]:
-                if self.shoot_angle > self.lo_angle:
-                    self.shoot_angle -= 1
-                    arrow.rotation_angle = self.shoot_angle
+                keys = pygame.key.get_pressed()
+
+                # manual key inputs
+                if keys[pygame.K_LEFT]:
+                    if self.strength > self.lo_strength_cap:
+                        self.strength -= 1
+                if keys[pygame.K_RIGHT]:
+                    if self.strength < self.up_strength_cap:
+                        self.strength += 1
+                if keys[pygame.K_SPACE]:
+                    if not arrow.shot:
+                        # fire arrow
+                        arrow.shot = True
+                        # set strength values from rotation and shoot angle
+                        vel_x, vel_y = Arrow2DMathematics.get_velocity_x_velocity_y(self.strength, self.shoot_angle)
+                        arrow.vel_x = vel_x
+                        arrow.vel_y = vel_y
+                if keys[pygame.K_UP]:
+                    if self.shoot_angle < self.up_angle:
+                        self.shoot_angle += 1
+                        arrow.rotation_angle = self.shoot_angle
+                if keys[pygame.K_DOWN]:
+                    if self.shoot_angle > self.lo_angle:
+                        self.shoot_angle -= 1
+                        arrow.rotation_angle = self.shoot_angle
+
+                # Now let's draw all the sprites in one go. (For now we only have 1 sprite!)
+
+                self.canvas.blit(self.strength_text, (0, 0))
+                self.canvas.blit(self.shoot_angle_text, (100, 0))
+
+                # check for finished arrows
+                if keys[pygame.K_r]:
+                    self.reset_arrows()
 
             # Game Logic
-            all_sprites_list.update()
+            self.all_sprites_list.update()
             # self.canvas.fill(GREEN)
             self.canvas.blit(self.back_ground_img, (0, 0))
             self.canvas.blit(self.ballista_img, (0, self.height - 64))
 
-            # Now let's draw all the sprites in one go. (For now we only have 1 sprite!)
-            all_sprites_list.draw(self.canvas)
-            self.canvas.blit(self.strength_text, (0, 0))
-            self.canvas.blit(self.shoot_angle_text, (100, 0))
-
-            # check for finished arrows
-            if keys[pygame.K_r]:
-                self.reset_arrows()
-
-            # DEBUG STATES
-            color = (255, 0, 0)
-            # ellipse = pygame.draw.ellipse(self.canvas, color, target.ellipse)
-            # bounding_rectangle_projectile = pygame.draw.rect(self.canvas, color, arrow.collision_head)
-
-            # pygame.display.update()
+            self.all_sprites_list.draw(self.canvas)
             pygame.display.flip()
             clock.tick(40)
         pygame.quit()
--- src/simulation_2d/sprites/projectile.py
+++ src/simulation_2d/sprites/projectile.py
@@ -1,5 +1,7 @@
 import pygame
 import math
+import random
+
 import numpy as np
 
 from src.simulation_2d import constants
@@ -16,6 +18,12 @@ class Projectile(pygame.sprite.Sprite):
             border_y: int,
             im_filepath: str,
             target,
+            up_streng_limit: int,
+            lo_strength_limit: int,
+            up_angle_imit: int,
+            lo_angle_limit: int,
+            male=None,
+            female=None,
     ):
         """
         central class for the projectile which is being shot
@@ -28,6 +36,11 @@ class Projectile(pygame.sprite.Sprite):
         """
         super().__init__()
         color = (0, 0, 255)
+        self.up_streng_limit = up_streng_limit
+        self.lo_strength_limit = lo_strength_limit
+        self.up_angle_imit = up_angle_imit
+        self.lo_angle_limit = lo_angle_limit
+
         self.height = 20
         self.width = 20
         self.image = pygame.Surface([self.width, self.height])
@@ -51,20 +64,31 @@ class Projectile(pygame.sprite.Sprite):
         self.shot = False
         self.finished = False
 
-        # self.rect.left += 30
-        # self.rect.top += 15
-        # self.rect.height -= 10
         self.rect.width -= 55
         self.rect.left += 200
-        # self.rect.x += 100
         self.rect.x = 0
-        # self.rect.y = self.border_y - self.height * 2
         self.rect.y = self.border_y - 55
 
         self.collision_head_x_offset = 40
         self.collision_head = None
         self.create_collision_head()
         self.score = 0.0
+
+        # genetic algorithm parts
+        self.learning_rate = 0.005
+
+        if male is None:  # New Bird, no parents
+            # easy network
+            self.inputWeights = np.random.normal(0, scale=0.1, size=(2, 3))
+            self.hiddenWeights = np.random.normal(0, scale=0.1, size=(3, 2))
+        elif female is None:  # Only one Parent (self mutate)
+            self.inputWeights = male.inputWeights
+            self.hiddenWeights = male.hiddenWeights
+            self.mutate()
+        else:  # Two parents - Breed.
+            self.inputWeights = np.random.normal(0, scale=0.1, size=(2, 3))
+            self.hiddenWeights = np.random.normal(0, scale=0.1, size=(3, 2))
+            self.breed(male, female)
 
     def reset_arrow(self):
         """
@@ -81,6 +105,10 @@ class Projectile(pygame.sprite.Sprite):
         self.create_collision_head()
 
     def create_collision_head(self):
+        """
+        create collision head for the simulation
+        :return:
+        """
         self.collision_head = self.rect.copy()
         self.collision_head.x += self.collision_head_x_offset
         self.collision_head.height -= 10
@@ -103,22 +131,73 @@ class Projectile(pygame.sprite.Sprite):
         """
         assert self.finished
         # check if is collided with the target
+        self.score = 0.0
+        center_of_target_x = int(self.target.ellipse[0] + 0.5 * self.target.ellipse[2])
+        center_of_target_y = int(self.target.ellipse[1] + 0.5 * self.target.ellipse[3])
+
+        coll_head_x = int(self.collision_head[0] + 0.5 * self.collision_head[2])
+        coll_head_y = int(self.collision_head[1] + 0.5 * self.collision_head[3])
+        distance_x = abs(center_of_target_x - coll_head_x)
+        distance_y = abs(center_of_target_y - coll_head_y)
+        distance = math.sqrt(math.pow(distance_x, 2) + math.pow(distance_y, 2))
+        max_dist = math.sqrt(math.pow(self.target.ellipse[2], 2) + math.pow(self.target.ellipse[3], 2))
+        self.score = 1 - distance / max_dist
+        self.score = 200 / distance
+
         if self.check_collision(self.target):
-            self.score = 2.0
-            center_of_target_x = int(self.target.ellipse[0] + 0.5 * self.target.ellipse[2])
-            center_of_target_y = int(self.target.ellipse[1] + 0.5 * self.target.ellipse[3])
+            self.score += 200
 
-            coll_head_x = int(self.collision_head[0] + 0.5 * self.collision_head[2])
-            coll_head_y = int(self.collision_head[1] + 0.5 * self.collision_head[3])
-            distance_x = abs(center_of_target_x - coll_head_x)
-            distance_y = abs(center_of_target_y - coll_head_y)
-            distance = math.sqrt(math.pow(distance_x, 2) + math.pow(distance_y, 2))
-            max_dist = math.sqrt(math.pow(self.target.ellipse[2], 2) + math.pow(self.target.ellipse[3], 2))
-            self.score = 1 - distance / max_dist
-
-        else:
-            self.score = 0.0
         print(self.score)
+        return self.score
+
+    @staticmethod
+    def relu(x):
+        """The relu activation function for the neural network
+
+        INPUT: x - The value to apply the ReLu function on
+        OUTPUT: The applied ReLus function value"""
+
+        return np.maximum(x, 0)
+
+    @staticmethod
+    def sigmoid(x):
+        """The sigmoid activation function for the neural net
+
+        INPUT: x - The value to calculate
+        OUTPUT: The calculated result"""
+
+        return 1 / (1 + np.exp(-x))
+
+    def set_weights(self, input_weights, hidden_weights):
+        """Overwrites the current weights of the birds brain (neural network).
+
+        INPUT:  inputWeights: The weights for the neural network (input layer)
+                hiddenWeights: The weights for the neural network (hidden layer)
+        OUTPUT:	None"""
+        self.inputWeights = input_weights
+        self.hiddenWeights = hidden_weights
+
+    def make_shot_decision(self):
+        # get inputs for the algorithm
+        center_of_target_x = int(self.target.ellipse[0] + 0.5 * self.target.ellipse[2])
+        center_of_target_y = int(self.target.ellipse[1] + 0.5 * self.target.ellipse[3])
+
+        coll_head_x = int(self.collision_head[0] + 0.5 * self.collision_head[2])
+        coll_head_y = int(self.collision_head[1] + 0.5 * self.collision_head[3])
+        distance_x = abs(center_of_target_x - coll_head_x)
+        distance_y = abs(center_of_target_y - coll_head_y)
+
+        x = [distance_x, distance_y]
+
+        hidden_layer_in = np.dot(x, self.inputWeights)
+        hidden_layer_out = Projectile.sigmoid(hidden_layer_in)
+        output_layer_in = np.dot(hidden_layer_out, self.hiddenWeights)
+        prediction = self.sigmoid(output_layer_in)
+
+        # share strength
+        strength_value = round(self.lo_strength_limit + (self.up_streng_limit - self.lo_strength_limit) * prediction[0])
+        angle_value = round(self.lo_angle_limit + (self.up_angle_imit - self.lo_angle_limit) * prediction[1])
+        return strength_value, angle_value
 
     def update(self):
         """
@@ -158,4 +237,53 @@ class Projectile(pygame.sprite.Sprite):
         self.collision_head.x = self.rect.x + self.collision_head_x_offset
         self.collision_head.y = self.rect.y
 
-        print('x: {0}, y: {1}'.format(self.rect.y, self.rect.x))
+        # print('x: {0}, y: {1}'.format(self.rect.y, self.rect.x))
+
+    def mutate(self):
+        """mutate (randomly apply the learning rate) the birds brain
+            (neural network) randomly changing the individual weights
+
+        INPUT:  None
+        OUTPUT:	None"""
+        for i in range(len(self.inputWeights)):
+            for j in range(len(self.inputWeights[i])):
+                self.inputWeights[i][j] = self.get_mutated_gene(self.inputWeights[i][j])
+        for i in range(len(self.hiddenWeights)):
+            for j in range(len(self.hiddenWeights[i])):
+                self.hiddenWeights[i][j] = self.get_mutated_gene(self.hiddenWeights[i][j])
+
+    # genetic algorithm code
+    def get_mutated_gene(self, weight):
+        """mutate the input by -0.125 to 0.125 or not at all
+
+        INPUT: weight - The weight to mutate
+        OUTPUT: mutatedWeight - The mutated weight"""
+
+        multiplier = 0
+        learning_rate = random.randint(0, 25) * self.learning_rate
+        rand_bool = bool(random.getrandbits(1))  # adapt upwards or downwards?
+        rand_bool2 = bool(random.getrandbits(1))  # or not at all?
+
+        if rand_bool and rand_bool2:
+            multiplier = 1
+        elif not rand_bool and rand_bool2:
+            multiplier = -1
+
+        mutated_weight = weight + learning_rate * multiplier
+
+        return mutated_weight
+
+    def breed(self, male, female):
+        """Generate a new brain (neural network) from two parent birds
+             by averaging their brains and mutating them afterwards
+
+        INPUT:  male - The male bird object (of class bird)
+                female - The female bird object (of class bird)
+        OUTPUT:	None"""
+        for i in range(len(self.inputWeights)):
+            self.inputWeights[i] = (male.inputWeights[i] +
+                                    female.inputWeights[i]) / 2
+
+        for i in range(len(self.hiddenWeights)):
+            self.hiddenWeights[i] = (male.hiddenWeights[i] +
+                                     female.hiddenWeights[i]) / 2
